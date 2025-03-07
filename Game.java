cb96a650fb11d2c7829890d1f6805102f3ce13d4
Simangaliso Phakwe <siphakwjhb024@student.wethinkcode.co.za>
2025-03-07 22:40:15 +0200

    Promted user for selection after showing options

--- Game.java
+++ Game.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Random;
 import java.util.Scanner;
@@ -33,9 +34,20 @@ public class Game {
         String[] spock = {"Rock", "Scissor"};
         options.put("Spock", spock);
 
-
-
+        // Keep a copy of options as a list, Trust me
+        ArrayList<String> list = new ArrayList<>(options.keySet());
         
+        // Display options to user
+        System.out.println("Please pick a hand to play");
+        for (String option: list) {
+            System.out.printf("%d. %s\n", enumerate, option);
+            enumerate++;
+        }
+        System.out.print(">> ");
+        userChoice = scanner.nextInt() - 1;
+        scanner.nextLine();
+
+
         scanner.close();
         
     }
