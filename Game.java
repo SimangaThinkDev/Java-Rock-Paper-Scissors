0838e08855bacd0050974af85188f5af407f8184
Simangaliso Phakwe <siphakwjhb024@student.wethinkcode.co.za>
2025-03-07 22:39:16 +0200

    Created the Game Rules

--- Game.java
+++ Game.java
@@ -1,5 +1,42 @@
+import java.util.HashMap;
+import java.util.Random;
+import java.util.Scanner;
+
 public class Game {
 
-    
+    static Scanner scanner = new Scanner(System.in);
+    public static void main(String[] args) {
+        
+        // Main code here
+        HashMap<String, String[]> options = new HashMap<>();
+        Random random = new Random();
 
+        // Declarations
+        int userChoice;
+        int computerChoice = random.nextInt(5);
+        int enumerate = 1; // you should know by now why I am doing this
+
+        // Add all the options
+        //Rock
+        String[] rock = {"Scissor", "Lizard"};
+        options.put("Rock", rock);
+        //Paper
+        String[] paper = {"Spock", "Rock"};
+        options.put("Paper", paper);
+        //Scissor
+        String[] scissor = {"Paper", "Lizard"};
+        options.put("Scissor", scissor);
+        //Lizard
+        String[] lizard = {"Paper", "Spock"};
+        options.put("Lizard", lizard);
+        //Spock
+        String[] spock = {"Rock", "Scissor"};
+        options.put("Spock", spock);
+
+
+
+        
+        scanner.close();
+        
+    }
 }
